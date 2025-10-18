e8b7d8cb12648629015382cb3840f4c46052633a
unknown <hradoevsoft@gmail.com>
2025-10-18 15:38:30 +0300

    add server

---
+++ testpythonaspng.server/Models/MyDependencyInjection.cs
@@ -0,0 +1,157 @@
+﻿namespace testpythonaspng.Server.Models
+{
+    public class MyDependencyInjection
+    {
+    }
+    public interface ILog
+    {
+        void info(string str);
+    }
+
+    class MyConsoleLogger : ILog
+    {
+        public void info(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+
+    public struct IpInfo
+    {
+        public string ip;
+        public bool is_not_allowed;
+    }
+
+    public interface IMyLogger
+    {
+        IpInfo GetUserIP(Microsoft.AspNetCore.Http.HttpContext context);
+        void WriteLog(Microsoft.AspNetCore.Http.HttpContext context, string log_text);
+        void WriteMsg(string msg);
+        void Log(string logMessage, TextWriter w);
+
+    }
+
+    class SessionInfo
+    {
+        private string key; // field
+        private string name; // field
+        public string Key   // property
+        {
+            get { return key; }
+            set { key = value; }
+        }
+        public string Name   // property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public SessionInfo(string k, string v)
+        {
+            this.Key = k;
+            this.Name = v;
+        }
+    }
+
+    public interface ISessionList
+    {
+        void AddSessionId(string SessionKey, string SessionName);
+        string GetSessionId(int index);
+
+
+    }
+
+    public class MyLogger : IMyLogger
+    {
+        //string logFile = @"Log\hrembros_log.txt";
+        //string logFile = Path.Combine(Directory.GetCurrentDirectory() + "\\Log\\","hrembros_log.txt");
+
+        private string logFile = "MyLogger.txt";
+        string[] not_allowed = { "46.10.", "87.126.", "90.154.", "151.251.", "66.249." };
+
+        public MyLogger()
+        {
+
+            if (!(File.Exists(logFile)))
+            {
+                File.Create(logFile);
+            }
+        }
+
+        public IpInfo GetUserIP(Microsoft.AspNetCore.Http.HttpContext context)
+        {
+            //TODO Can I get remote client ip addres;
+            IpInfo ipInfo;
+            ipInfo.ip = Convert.ToString(context.Connection.RemoteIpAddress);
+            ipInfo.is_not_allowed = false;
+
+            for (int i = 0; i <= not_allowed.Length - 1; i++)
+            {
+                ipInfo.is_not_allowed = ipInfo.ip.Contains(not_allowed[i]);
+                if (ipInfo.is_not_allowed)
+                {
+                    break;
+                }
+
+            }
+            return ipInfo;
+        }
+
+
+
+        public async void WriteLog(Microsoft.AspNetCore.Http.HttpContext context, string log_text)
+        {
+            IpInfo ipInfo = GetUserIP(context);
+            if (!ipInfo.is_not_allowed)
+            {
+
+                using (StreamWriter w = File.AppendText(logFile))
+                {
+                    await Task.Run(() => Log(log_text + " :ip = " + ipInfo.ip, w));
+                }
+
+            }
+            else
+            {
+                using (StreamWriter w = File.AppendText(logFile))
+                {
+                    await Task.Run(() => Log(log_text + " :ip = " + ipInfo.ip + " :self or google", w));
+                }
+            }
+        }
+
+        public async void WriteMsg(string msg)
+        {
+            using (StreamWriter w = File.AppendText(logFile))
+            {
+                await Task.Run(() => Log(msg, w));
+            }
+        }
+
+        public void Log(string logMessage, TextWriter w)
+        {
+            w.Write($"{DateTime.Now}");
+            w.WriteLine($"  :{logMessage}");
+            w.WriteLine("-----------------");
+        }
+    }
+
+
+    public class SessionList : ISessionList
+    {
+        List<SessionInfo> sessionInfo = new List<SessionInfo>();
+        public void AddSessionId(string SessionKey, string SessionName)
+        {
+            this.sessionInfo.Add(new SessionInfo(SessionKey, SessionName));
+        }
+
+        public string GetSessionId(int index)
+        {
+            SessionInfo sessInfo = this.sessionInfo.ElementAt(index);
+            return sessInfo.Key;
+
+        }
+    }
+
+}
---
+++ testpythonaspng.server/Program.cs
@@ -0,0 +1,75 @@
+using CSnakes.Runtime;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.DependencyInjection;
+using testpythonaspng.Server.Models;
+/*using Microsoft.Extensions.Hosting;
+using testpythonaspmvc.Models;
+using testpythonaspmvc;*/
+var builder = WebApplication.CreateBuilder(args);
+MyLogger myLogger = new MyLogger();
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+var home = Path.Join(Environment.CurrentDirectory, "python");
+var python_src = Path.Join(AppContext.BaseDirectory) + "csnakes\\python3.12.9\\python\\install\\";
+var venv_path = Path.Join(AppContext.BaseDirectory) + "python\\.venv";
+//var python_src = "C:\\Users\\HRR\\AppData\\Roaming\\CSnakes\\python3.12.9\\python\\install\\";
+myLogger.WriteMsg(python_src);
+
+builder.Services
+    .WithPython()
+    .WithHome(home)
+    /*.WithVirtualEnvironment(venv_path)
+    .WithPipInstaller()*/
+    //.FromFolder(@python_src, "3.12");
+    .FromRedistributable("3.12"); // Downloads Python automatically
+                            //.FromFolder(@python_src, "3.12");
+var app = builder.Build();
+try
+{
+    var env = app.Services.GetRequiredService<IPythonEnvironment>();
+    var paythonService = env.Example1();
+    string test_py = paythonService.HelloWorld("Radka", 80);
+    myLogger.WriteMsg(test_py);
+}
+catch (Exception e)
+{
+    myLogger.WriteMsg(e.Message);
+
+}
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+   /* app.UseSwagger();
+    app.UseSwaggerUI();*/
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthorization();
+
+//app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
