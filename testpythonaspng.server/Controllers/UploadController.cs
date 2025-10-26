3de5c87a28b5bd188c0c54f99fef02c35542cbed
unknown <hradoevsoft@gmail.com>
2025-10-26 16:39:20 +0200

    python get_table_columns

---
+++ testpythonaspng.server/Controllers/UploadController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace testpythonaspng.Server.Controllers
+{
+    public class MyDto
+    {
+
+       // public IFormFile File { get; set; }
+       public string name { get; set; }
+       public IFormFile File { get; set; }
+    } 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> UploadData([FromForm] MyDto myDto)
+        {
+            IFormFile file = Request.Form.Files[0];
+            string fileName = myDto.name;
+            string fullPath = Path.Combine("uploads/", fileName);
+
+            var buffer = 1024 * 1024;
+            using var stream = new FileStream(fullPath, FileMode.Create, FileAccess.Write, FileShare.None, buffer, useAsync: false);
+            await file.CopyToAsync(stream);
+            await stream.FlushAsync();
+            string result = "OK";
+            
+            return Ok(result);
+        }
+    }
+}
--- testpythonaspng.server/python/exploratory.py
+++ testpythonaspng.server/python/exploratory.py
@@ -1,9 +1,16 @@
 ﻿import pandas as pd
 import numpy as np
 
-def get_columns(filename: str)  -> list[dict[str,str]]:
+def example_return_columns_dict(filename: str)  -> list[dict[str,str]]:
     return [{"data": "AAA", "title": "AAA"}, {"data": "BBB", "title": "BBB"}] 
 
+def get_columns(filename: str)  -> list[dict[str,str]]:
+    explor_df = pd.read_csv('uploads/product_vending_analisys.csv', encoding='cp1251',  sep=',')
+    explor_columns = explor_df.columns
+    dtable_columns = []
+    for column in explor_columns:
+        dtable_columns.append({"data": column, "title": column})
+    return  dtable_columns
 
 
 
